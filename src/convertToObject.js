d73c21ca84210a5784b2b0247f64dbb5e25aab92
Olga Bilokur <olyanderbilokur@gmail.com>
2026-04-06 00:02:43 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,20 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const array = sourceString.split(';');
+  const convertingObject = {};
+
+  array.forEach((el) => {
+    if (!el.trim()) {
+      return; // пропускаємо пусті рядки
+    }
+
+    const [key, value] = el.split(':');
+
+    convertingObject[key.trim()] = value.trim();
+  });
+
+  return convertingObject;
 }
 
 module.exports = convertToObject;
